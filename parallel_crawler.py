0ea93dbccf216e08522621353c0ecd0681654b4e
Shining Xu <xushangning@sjtu.edu.cn>
2018-10-24 22:59:18 +0800

    Rewrite parallel_crawler.py with OOP

--- parallel_crawler.py
+++ parallel_crawler.py
@@ -1,53 +1,114 @@
+import os
+import re
 import threading
 from queue import Queue
 
-from bloomfilter import BloomFilter
-import crawler
+import requests
+from bs4 import BeautifulSoup
+
 from GeneralHashFunctions import *
-
-seed = 'https://www.sjtu.edu.cn'
-q = Queue()
-max_page = 2000
-count_pages = 0
-graph = {}
-
-BIT_ARRAY_SIZE = 16384
-my_filter = BloomFilter(BIT_ARRAY_SIZE, hash, RSHash, JSHash, SDBMHash, FNVHash)
-
-N_THREADS = 4
-var_lock = threading.Lock()
+from bloomfilter import BloomFilter
 
 
-def crawling():
-    global count_pages
-    while count_pages < max_page:
-        print(count_pages, end=' ')
-        url = q.get()
-        if not my_filter.query(url):
-            print(url)
-            content = crawler.get_page(url)
-            if len(content):
-                my_filter.set(url)
-                crawler.add_page_to_folder(url, content)
-                outlinks = crawler.get_all_links(content, url)
-                var_lock.acquire()  # ready to change the global environment
-                count_pages += 1
-                if len(outlinks):
-                    graph[url] = outlinks
-                    for link in outlinks:
-                        q.put(link)
-                var_lock.release()
-        q.task_done()
+class CrawledDoc:
+    def __init__(self, url=''):
+        self.text = ''
+        self.url = url
 
 
-q.put(seed)
-thread_pool = []
-for _ in range(N_THREADS):
-    t = threading.Thread(target=crawling)
-    t.start()
-    thread_pool.append(t)
-for t in thread_pool:
-    t.join()
+class CrawlerThread(threading.Thread):
+    pages_count = 0
+    max_pages = 10000
+    queue = Queue()
+    filter = BloomFilter(131072, False, RSHash, hash, JSHash, SDBMHash, FNVHash)
+    lock = threading.Lock()
+    valid_char_in_filename = '-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-my_filter.print_stats()
-print("Number of Crawled Pages:", count_pages)
+    def __init__(self, index_file):
+        self.index_file = index_file
+        self.doc = CrawledDoc()
+        super().__init__()
+
+    def run(self):
+        cls = self.__class__
+        while cls.pages_count < cls.max_pages:
+            self.doc = CrawledDoc(cls.queue.get())
+            cls.queue.task_done()
+            if 'sjtu' in self.doc.url and self.is_html():
+                try:
+                    r = requests.get(self.doc.url, timeout=1)
+                    content_type = r.headers['Content-Type']
+                    charset_pos = content_type.find('charset=')
+                    soup = BeautifulSoup(features='lxml')
+                    if charset_pos == -1:
+                        # let Beautiful Soup determine encoding from <meta>
+                        soup = BeautifulSoup(r.content, 'lxml')
+                    else:
+                        # len("charset=") == 8
+                        r.encoding = content_type[charset_pos + 8:]
+                        soup = BeautifulSoup(r.text, 'lxml')
+
+                    for tag in soup.find_all(['script', 'style']):
+                        tag.decompose()
+                    try:
+                        # detect parsing errors after <script> and <style>
+                        # were removed
+                        repr(soup)
+                    except RecursionError as e:
+                        print(self.name + ': ' + self.doc.url + '\n   ', e)
+                        continue
+
+                    self.doc.text = soup.get_text('\n', strip=True)
+                    if len(self.doc.text):
+                        print(self.name + ':', cls.pages_count, self.doc.url)
+                        try:
+                            title = soup.find('title').string.strip()
+                        except AttributeError:
+                            title = self.doc.url
+                            print(self.name + ': ' + self.doc.url)
+                            print('    AttributeError: the webpage doesn\'t have a title')
+
+                        filename = self.url2filename()
+                        with open('crawled/html/' + filename, 'w') as f:
+                            f.write(str(soup))
+                        with open('crawled/text/' + filename, 'w') as f:
+                            f.write(self.doc.text)
+
+                        cls.lock.acquire()
+                        self.index_file.write(self.doc.url
+                                              + '\t' + filename
+                                              + '\t' + title + '\n')
+                        for a in soup.find_all('a', href=re.compile('^https?')):
+                            cls.queue.put(a['href'])
+                        cls.pages_count += 1
+                        cls.lock.release()
+
+                except requests.exceptions.RequestException as e:
+                    print(self.name + ': ' + self.doc.url + '\n   ', e)
+
+    def is_html(self):
+        try:
+            r = requests.head(self.doc.url, timeout=1)
+            return 'text/html' in r.headers.get('Content-Type', '')
+        except requests.exceptions.RequestException as e:
+            print(self.name + ': ' + self.doc.url + '\n   ', e)
+            return False
+
+    def url2filename(self):
+        return ''.join(c for c in self.doc.url if c in self.__class__.valid_char_in_filename)[:64]
+
+
+if __name__ == '__main__':
+    if not os.path.exists('crawled/html'):
+        os.mkdir('crawled/html')
+    if not os.path.exists('crawled/text'):
+        os.mkdir('crawled/text')
+    CrawlerThread.queue.put('https://www.sjtu.edu.cn')
+    thread_pool = []
+    N_THREADS = 1
+    index_file = open('crawled/index.txt', 'w')
+
+    for _ in range(N_THREADS):
+        t = CrawlerThread(index_file)
+        t.start()
+        thread_pool.append(t)
