96cd774fe7b31901e7f23c3acd55f04b9b97ac41
Shining Xu <xushangning@sjtu.edu.cn>
2018-10-25 22:46:00 +0800

    Port Lucene examples to Lucene 7.5.0

--- IndexFiles.py
+++ IndexFiles.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
-
-INDEX_DIR = "IndexFiles.index"
-
-import sys, os, lucene, threading, time
+import lucene
+import os
+import sys
+import threading
+import time
 from datetime import datetime
 
 from java.io import File
 from org.apache.lucene.analysis.miscellaneous import LimitTokenCountAnalyzer
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document, Field, FieldType
-from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig
+from org.apache.lucene.index import IndexWriter, IndexWriterConfig, IndexOptions
 from org.apache.lucene.store import SimpleFSDirectory
-from org.apache.lucene.util import Version
 
 """
 This class is loosely based on the Lucene (java implementation) demo class 
@@ -21,6 +20,9 @@ It will index on the file path, the file name and the file contents.  The
 resulting Lucene index will be placed in the current directory and called
 'index'.
 """
+
+INDEX_DIR = "IndexFiles.index"
+
 
 class Ticker(object):
 
@@ -33,6 +35,7 @@ class Ticker(object):
             sys.stdout.flush()
             time.sleep(1.0)
 
+
 class IndexFiles(object):
     """Usage: python IndexFiles <doc_directory>"""
 
@@ -41,10 +44,10 @@ class IndexFiles(object):
         if not os.path.exists(storeDir):
             os.mkdir(storeDir)
 
-        store = SimpleFSDirectory(File(storeDir))
-        analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
+        store = SimpleFSDirectory(File(storeDir).toPath())
+        analyzer = StandardAnalyzer()
         analyzer = LimitTokenCountAnalyzer(analyzer, 1048576)
-        config = IndexWriterConfig(Version.LUCENE_CURRENT, analyzer)
+        config = IndexWriterConfig(analyzer)
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
         writer = IndexWriter(store, config)
 
@@ -60,15 +63,13 @@ class IndexFiles(object):
     def indexDocs(self, root, writer):
 
         t1 = FieldType()
-        t1.setIndexed(False)
         t1.setStored(True)
         t1.setTokenized(False)
         
         t2 = FieldType()
-        t2.setIndexed(True)
         t2.setStored(False)
         t2.setTokenized(True)
-        t2.setIndexOptions(FieldInfo.IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+        t2.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
         
         for root, dirnames, filenames in os.walk(root):
             for filename in filenames:
@@ -78,7 +79,7 @@ class IndexFiles(object):
                 try:
                     path = os.path.join(root, filename)
                     file = open(path)
-                    contents = str(file.read(), 'gbk')
+                    contents = file.read()
                     file.close()
                     doc = Document()
                     doc.add(Field("name", filename, t1))
@@ -91,12 +92,14 @@ class IndexFiles(object):
                 except Exception as e:
                     print("Failed in indexDocs:", e)
 
+
 if __name__ == '__main__':
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print('lucene', lucene.VERSION)
     start = datetime.now()
     try:
-        IndexFiles('testfolder', "index")
+        prefix = 'instructions/4/samples4/'
+        IndexFiles(prefix + 'testfolder', "index")
         end = datetime.now()
         print(end - start)
     except Exception as e:
--- SearchFiles.py
+++ SearchFiles.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-
-INDEX_DIR = "IndexFiles.index"
-
-import sys, os, lucene
-
+import lucene
 from java.io import File
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.index import DirectoryReader
 from org.apache.lucene.queryparser.classic import QueryParser
-from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
-from org.apache.lucene.util import Version
+from org.apache.lucene.store import SimpleFSDirectory
 
 """
 This script is loosely based on the Lucene (java implementation) demo class 
@@ -21,19 +15,21 @@ search query entered against the 'contents' field.  It will then display the
 search.close() is currently commented out because it causes a stack overflow in
 some cases.
 """
+
+INDEX_DIR = "IndexFiles.index"
+
+
 def run(searcher, analyzer):
     while True:
         print()
         print("Hit enter with no input to quit.")
         command = input("Query:")
-        command = str(command, 'GBK')
         if command == '':
             return
 
         print()
         print("Searching for:", command)
-        query = QueryParser(Version.LUCENE_CURRENT, "contents",
-                            analyzer).parse(command)
+        query = QueryParser("contents", analyzer).parse(command)
         scoreDocs = searcher.search(query, 50).scoreDocs
         print("%s total matching documents." % len(scoreDocs))
 
@@ -48,8 +44,8 @@ if __name__ == '__main__':
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print('lucene', lucene.VERSION)
     #base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    directory = SimpleFSDirectory(File(STORE_DIR))
+    directory = SimpleFSDirectory(File(STORE_DIR).toPath())
     searcher = IndexSearcher(DirectoryReader.open(directory))
-    analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
+    analyzer = StandardAnalyzer()
     run(searcher, analyzer)
     del searcher
