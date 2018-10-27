b9e2e234659a12613e4fb14a6c46d856f5e3df94
Shining Xu <xushangning@sjtu.edu.cn>
2018-10-27 19:43:55 +0800

    Crawl Duokan to build an image index

--- parallel_crawler.py
+++ parallel_crawler.py
@@ -1,4 +1,3 @@
-import os
 import threading
 from queue import Queue
 import urllib.parse
@@ -12,13 +11,13 @@ from bloomfilter import BloomFilter
 
 class CrawledDoc:
     def __init__(self, url=''):
-        self.text = ''
+        self.src = ''
         self.url = url
 
 
 class CrawlerThread(threading.Thread):
     pages_count = 0
-    max_pages = 10000
+    max_pages = 40000
     queue = Queue()
     filter = BloomFilter(1048576, False, RSHash, hash, JSHash, SDBMHash, FNVHash)
     lock = threading.Lock()
@@ -34,7 +33,7 @@ class CrawlerThread(threading.Thread):
         while cls.pages_count < cls.max_pages:
             self.doc = CrawledDoc(cls.queue.get())
             cls.queue.task_done()
-            if ('sjtu' in self.doc.url
+            if ('duokan' in self.doc.url
                     and not self.filter.query(self.doc.url)
                     and self.is_html()):
                 try:
@@ -49,53 +48,33 @@ class CrawlerThread(threading.Thread):
                     else:
                         soup = BeautifulSoup(r.text, 'html5lib')
 
-                    for tag in soup.find_all(['script', 'style']):
-                        tag.decompose()
                     try:
-                        # detect parsing errors after <script> and <style>
-                        # were removed
-                        repr(soup)
-                    except RecursionError as e:
-                        print(self.name + ': ' + self.doc.url + '\n   ', e)
-                        continue
+                        title = soup.find('title').string.strip()
+                    except AttributeError:
+                        title = self.doc.url
+                        print(self.name + ': ' + self.doc.url)
+                        print('    AttributeError: the webpage doesn\'t have a title')
 
-                    self.doc.text = soup.get_text('\n', strip=True)
-                    if len(self.doc.text):
-                        self.filter.set(self.doc.url)
-                        print(self.name + ':', cls.pages_count, self.doc.url)
-                        try:
-                            title = soup.find('title').string.strip()
-                        except AttributeError:
-                            title = self.doc.url
-                            print(self.name + ': ' + self.doc.url)
-                            print('    AttributeError: the webpage doesn\'t have a title')
-
-                        filename = self.url2filename()
-                        with open('crawled/html/' + filename, 'w') as f:
-                            f.write(str(soup))
-                        with open('crawled/text/' + filename, 'w') as f:
-                            f.write(self.doc.text)
-
-                        cls.lock.acquire()
-                        self.index_file.write(self.doc.url
-                                              + '\t' + filename
-                                              + '\t' + title + '\n')
-
-                        # add URLs in the web page to the queue
-                        # Duplicate URLs are not removed in this stage.
-                        for a in soup.find_all('a', href=True):
-                            link = ''.join(c for c in a['href'] if not c.isspace())
-                            if link.startswith('http'):
-                                cls.queue.put(link)
-                            elif link.startswith('/'):     # absolute path
-                                cls.queue.put(urllib.parse.urljoin(
-                                    self.doc.url, link))
-                            elif len(link):                # relative path
-                                if not self.doc.url.endswith('/'):
-                                    link = '/' + link
-                                cls.queue.put(self.doc.url + link)
+                    cls.lock.acquire()
+                    self.filter.set(self.doc.url)
+                    for img in soup.find_all('img', src=True, alt=True):
                         cls.pages_count += 1
-                        cls.lock.release()
+                        print(self.name + ':', cls.pages_count, img['src'])
+                        index_file.write(img['src'] + '\t' + img['alt'] + '\t' + title + '\n')
+                    # add URLs in the web page to the queue
+                    # Duplicate URLs are not removed in this stage.
+                    for a in soup.find_all('a', href=True):
+                        link = a['href'].rstrip('javascript:void(0)')
+                        if link.startswith('http'):
+                            cls.queue.put(link)
+                        elif link.startswith('/'):     # absolute path
+                            cls.queue.put(urllib.parse.urljoin(
+                                self.doc.url, link))
+                        elif len(link):                # relative path
+                            if not self.doc.url.endswith('/'):
+                                link = '/' + link
+                            cls.queue.put(self.doc.url + link)
+                    cls.lock.release()
 
                 # handle exceptions in request and bad responses
                 except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
@@ -114,16 +93,15 @@ class CrawlerThread(threading.Thread):
 
 
 if __name__ == '__main__':
-    if not os.path.exists('crawled/html'):
-        os.mkdir('crawled/html')
-    if not os.path.exists('crawled/text'):
-        os.mkdir('crawled/text')
-    CrawlerThread.queue.put('https://www.sjtu.edu.cn')
+    CrawlerThread.queue.put('https://www.duokan.com')
     thread_pool = []
     N_THREADS = 1
-    index_file = open('crawled/index.txt', 'w')
+    index_file = open('crawled/images/index.txt', 'a')
 
     for _ in range(N_THREADS):
         t = CrawlerThread(index_file)
         t.start()
         thread_pool.append(t)
+    for t in thread_pool:
+        t.join()
+    index_file.close()
