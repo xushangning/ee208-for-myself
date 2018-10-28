4c3175ac7181abd002391bb7f6949ef3d6f4c61b
Shining Xu <xushangning@sjtu.edu.cn>
2018-10-28 09:28:26 +0800

    Restructure IndexFiles.py
    Rewrite IndexFiles.py in a procedural style. Remove the unnecessary
    try...except...raise statement at the end of the file.

--- IndexFiles.py
+++ IndexFiles.py
@@ -27,63 +27,54 @@ class Ticker(object):
             time.sleep(1.0)
 
 
-class IndexFiles(object):
-    """Usage: python IndexFiles <doc_directory>"""
-
-    def __init__(self, storeDir):
-
-        if not os.path.exists(storeDir):
-            os.mkdir(storeDir)
-
-        store = SimpleFSDirectory(File(storeDir).toPath())
-        analyzer = WhitespaceAnalyzer()
-        analyzer = LimitTokenCountAnalyzer(analyzer, 1048576)
-        config = IndexWriterConfig(analyzer)
-        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
-        writer = IndexWriter(store, config)
-
-        self.indexDocs(writer)
-        ticker = Ticker()
-        print('commit index', end=' ')
-        threading.Thread(target=ticker.run).start()
-        writer.commit()
-        writer.close()
-        ticker.tick = False
-        print('done')
-
-    def indexDocs(self, writer):
-
-        filename_fieldtype = FieldType()
-        filename_fieldtype.setStored(True)
-        filename_fieldtype.setTokenized(False)
-        
-        title_fieldtype = FieldType()
-        title_fieldtype.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
-        title_fieldtype.setStored(True)
-
-        db = sqlite3.connect('index/images/image_index.sqlite')
-        cursor = db.cursor()
-        for row in cursor.execute('SELECT * FROM indices'):
-            url, description, title = row[:3]
-            print('Adding ' + description)
-            doc = Document()
-
-            doc.add(Field('description',
-                          ' '.join(jieba.cut_for_search(description)),
-                          title_fieldtype))
-            doc.add(Field('title', ' '.join(jieba.cut_for_search(title)), title_fieldtype))
-            doc.add(Field('url', url, filename_fieldtype))
-            writer.addDocument(doc)
-
-
 if __name__ == '__main__':
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print('lucene', lucene.VERSION)
     start = datetime.now()
-    try:
-        IndexFiles('index/images/')
-        end = datetime.now()
-        print(end - start)
-    except Exception as e:
-        print("Failed: ", e)
-        raise e
+
+    store_dir = 'index/images/'
+    if not os.path.exists(store_dir):
+        os.mkdir(store_dir)
+
+    # create an index writer
+    store = SimpleFSDirectory(File(store_dir).toPath())
+    analyzer = WhitespaceAnalyzer()
+    analyzer = LimitTokenCountAnalyzer(analyzer, 1048576)
+    config = IndexWriterConfig(analyzer)
+    config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+    writer = IndexWriter(store, config)
+
+    # add documents to the index
+    filename_fieldtype = FieldType()
+    filename_fieldtype.setStored(True)
+    filename_fieldtype.setTokenized(False)
+
+    title_fieldtype = FieldType()
+    title_fieldtype.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+    title_fieldtype.setStored(True)
+
+    db = sqlite3.connect('index/images/image_index.sqlite')
+    cursor = db.cursor()
+    for row in cursor.execute('SELECT * FROM indices'):
+        url, description, title = row[:3]
+        print('Adding ' + description)
+        doc = Document()
+
+        doc.add(Field('description',
+                      ' '.join(jieba.cut_for_search(description)),
+                      title_fieldtype))
+        doc.add(Field('title', ' '.join(jieba.cut_for_search(title)), title_fieldtype))
+        doc.add(Field('url', url, filename_fieldtype))
+        writer.addDocument(doc)
+
+    # commit index
+    ticker = Ticker()
+    print('commit index', end=' ')
+    threading.Thread(target=ticker.run).start()
+    writer.commit()
+    writer.close()
+    ticker.tick = False
+    print('done')
+
+    end = datetime.now()
+    print(end - start)
