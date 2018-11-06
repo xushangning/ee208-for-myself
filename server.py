d7cecd1998e5bad8e6c69a3a2840612e7deb1de3
Shining Xu <xushangning@sjtu.edu.cn>
2018-11-06 19:25:52 +0800

    Migrate to Flask

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import web
+import flask
 import jieba
 import lucene
 
@@ -11,14 +11,8 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search.highlight import (
     SimpleHTMLFormatter, Highlighter, QueryScorer, TokenSources)
 
-urls = (
-    '/', 'Index',
-    '/search', 'Search',
-    '/image', 'Image'
-)
-render = web.template.render('templates/', base='base')
-
-search_box = web.form.Form(web.form.Textbox('q', web.form.notnull))
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+app = flask.Flask(__name__)
 
 
 def parse_query(query):
@@ -44,87 +38,72 @@ def parse_query(query):
     return query_dict
 
 
-class Index:
-    def GET(self):
-        return render.index()
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
 
 
-class Search:
-    def GET(self):
-        search_box_copy = search_box()
-        if search_box_copy.validates():
-            lucene.getVMEnv().attachCurrentThread()
-            directory = SimpleFSDirectory(File('index/webpages/').toPath())
-            searcher = IndexSearcher(DirectoryReader.open(directory))
-            analyzer = WhitespaceAnalyzer()
+@app.route('/search')
+def search():
+    user_query = flask.request.args['q']
+    lucene.getVMEnv().attachCurrentThread()
+    directory = SimpleFSDirectory(File('index/webpages/').toPath())
+    searcher = IndexSearcher(DirectoryReader.open(directory))
+    analyzer = WhitespaceAnalyzer()
 
-            # In later version of Lucene, BooleanQuery becomes immutable and can
-            # only be constructed by BooleanQuery.Builder
-            query_builder = BooleanQuery.Builder()
-            user_query = search_box_copy['q'].value
-            query_dict = parse_query(user_query)
-            if query_dict.get('default') is not None:
-                terms = ' '.join(jieba.cut(query_dict['default']))
-                # search over "content" and "title' fields
-                query = QueryParser('content', analyzer).parse(terms)
-                query_builder.add(query, BooleanClause.Occur.SHOULD)
-                query = QueryParser('title', analyzer).parse(terms)
-                query_builder.add(query, BooleanClause.Occur.SHOULD)
-            if query_dict.get('site') is not None:
-                # use TermQuery because we expect an exact match
-                query_builder.add(TermQuery(Term('site', query_dict['site'])),
-                                  BooleanClause.Occur.MUST)
+    # In later version of Lucene, BooleanQuery becomes immutable and can
+    # only be constructed by BooleanQuery.Builder
+    query_builder = BooleanQuery.Builder()
+    query_dict = parse_query(user_query)
+    if query_dict.get('default') is not None:
+        terms = ' '.join(jieba.cut(query_dict['default']))
+        # search over "content" and "title' fields
+        query = QueryParser('content', analyzer).parse(terms)
+        query_builder.add(query, BooleanClause.Occur.SHOULD)
+        query = QueryParser('title', analyzer).parse(terms)
+        query_builder.add(query, BooleanClause.Occur.SHOULD)
+    if query_dict.get('site') is not None:
+        # use TermQuery because we expect an exact match
+        query_builder.add(TermQuery(Term('site', query_dict['site'])),
+                          BooleanClause.Occur.MUST)
 
-            # build a BooleanQuery object and pass it to searcher
-            # retrieve the top 10 hits
-            final_query = query_builder.build()
-            score_docs = searcher.search(final_query, 10).scoreDocs
-            results = []
-            for score_doc in score_docs:
-                doc = searcher.doc(score_doc.doc)
-                with open('crawled/text/' + doc.get('filename')) as f:
-                    text = ' '.join(jieba.cut_for_search(f.read()))
+    # build a BooleanQuery object and pass it to searcher
+    # retrieve the top 10 hits
+    final_query = query_builder.build()
+    score_docs = searcher.search(final_query, 10).scoreDocs
+    results = []
+    for score_doc in score_docs:
+        doc = searcher.doc(score_doc.doc)
+        with open('crawled/text/' + doc.get('filename')) as f:
+            text = ' '.join(jieba.cut_for_search(f.read()))
 
-                html_formatter = SimpleHTMLFormatter('<em>', '</em>')
-                highlighter = Highlighter(html_formatter, QueryScorer(final_query))
-                index_reader = searcher.getIndexReader()
-                token_stream = TokenSources.getTokenStream(
-                    'content', index_reader.getTermVectors(score_doc.doc),
-                    text, analyzer,
-                    highlighter.getMaxDocCharsToAnalyze() - 1
-                )
-                frag = highlighter.getBestFragment(token_stream, text)
-                results.append((doc.get('title'), frag))
-            return render.search(results)
-        else:
-            raise web.notfound()
+        html_formatter = SimpleHTMLFormatter('<em>', '</em>')
+        highlighter = Highlighter(html_formatter, QueryScorer(final_query))
+        index_reader = searcher.getIndexReader()
+        token_stream = TokenSources.getTokenStream(
+            'content', index_reader.getTermVectors(score_doc.doc),
+            text, analyzer,
+            highlighter.getMaxDocCharsToAnalyze() - 1
+        )
+        frag = highlighter.getBestFragment(token_stream, text)
+        results.append((doc.get('title'), flask.Markup(frag)))
+    return flask.render_template('search.html', results=results)
 
 
-class Image:
-    def GET(self):
-        search_box_copy = search_box()
-        if search_box_copy.validates():
-            lucene.getVMEnv().attachCurrentThread()
-            directory = SimpleFSDirectory(File('index/images/').toPath())
-            searcher = IndexSearcher(DirectoryReader.open(directory))
-            analyzer = WhitespaceAnalyzer()
+@app.route('/image')
+def image_search():
+    user_query = flask.request.args['q']
+    lucene.getVMEnv().attachCurrentThread()
+    directory = SimpleFSDirectory(File('index/images/').toPath())
+    searcher = IndexSearcher(DirectoryReader.open(directory))
+    analyzer = WhitespaceAnalyzer()
 
-            user_query = search_box_copy['q'].value
-            query = QueryParser('description', analyzer).parse(
-                ' '.join(jieba.cut(user_query)))
-            top_docs = searcher.search(query, 10).scoreDocs
+    query = QueryParser('description', analyzer).parse(
+        ' '.join(jieba.cut(user_query)))
+    top_docs = searcher.search(query, 10).scoreDocs
 
-            results = []
-            for score_doc in top_docs:
-                doc = searcher.doc(score_doc.doc)
-                results.append((doc.get('url'), doc.get('description'), doc.get('origin')))
-            return str(results)
-        else:
-            raise web.notfound()
-
-
-if __name__ == "__main__":
-    lucene.initVM(vmargs=['-Djava.awt.headless=true'])
-
-    app = web.application(urls, globals())
-    app.run()
+    results = []
+    for score_doc in top_docs:
+        doc = searcher.doc(score_doc.doc)
+        results.append((doc.get('url'), doc.get('description'), doc.get('origin')))
+    return str(results)
