2c1858a8de14cabcca00abd53556ef6ea1e80c13
Shining Xu <xushangning@sjtu.edu.cn>
2018-12-24 20:24:23 +0800

    A query on LSH table now returns filenames and distances

--- opencv/exercises/13/lsh_demo.py
+++ opencv/exercises/13/lsh_demo.py
@@ -74,40 +74,45 @@ class LSHTable:
             segmented.append(segment)
         return segmented
 
-    def add(self, v):
+    def add(self, v, data):
         """Add a vector to the hash table
         :param v: vector
+        :param data: data associated with v
         """
         for i, s in enumerate(self.map_sets):
             h = lsh(v, s)
             if self.table[i].get(h) is None:
-                self.table[i][h] = [v]
+                self.table[i][h] = [(v, data)]
             else:
-                self.table[i][h].append(v)
+                self.table[i][h].append((v, data))
 
     def knn(self, v, k):
         """
         Find k nearest neighbors
         :param v: vector
         :param k: number of neighbors
-        :return: list
+        :return: list that contains filenames and Euclidean distances between
+            the feature vector of this image and v
         """
+        # Each element in candidates is a tuple: ((vector, data), distance)
         candidates = []
         for i, s in enumerate(self.map_sets):
             # for each vector in the same bin
             for c in self.table[i][lsh(v, s)]:
                 # append to candidates if not a duplicate
                 for appended_candidate in candidates:
-                    if np.array_equal(appended_candidate[0], c):
+                    # if both have the same filename
+                    if c[1] == appended_candidate[0][1]:
                         break
                 else:
                     # second item is the Euclidean distance
-                    candidates.append((c, np.sqrt(np.square(v.astype(np.float64) - c).sum())))
+                    candidates.append((c, np.sqrt(np.square(v.astype(np.float64) - c[0]).sum())))
         candidates.sort(key=lambda c: c[1])
+        # return a list of filenames
         if len(candidates) > k:
-            return [candidates[i][0] for i in range(k)]
+            return [(candidates[i][0][1], candidates[i][1]) for i in range(k)]
         else:
-            return [c[0] for c in candidates]
+            return [(c[0][1], c[1]) for c in candidates]
 
 
 if __name__ == '__main__':
@@ -117,10 +122,11 @@ if __name__ == '__main__':
         [3, 5, 6, 9, 13, 19, 20, 22]
     ]
     t = LSHTable(map_sets, 2, 12)
+    directory = 'Dataset/'
     for i in range(1, 41):
-        filename = 'Dataset/{}.jpg'.format(i)
-        img = cv.imread(filename)
-        t.add(feature_vector(img))
+        filename = str(i) + '.jpg'
+        img = cv.imread(directory + filename)
+        t.add(feature_vector(img), filename)
     img = cv.imread('target.jpg')
     v = feature_vector(img)
     print(t.knn(v, 8))
