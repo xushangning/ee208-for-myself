d0d43f7bcc344a742e1f927e5c48388a920b2bd7
Shining Xu <xushangning@sjtu.edu.cn>
2018-12-24 19:57:27 +0800

    Baseline LSH

--- opencv/exercises/13/lsh_demo.py
+++ opencv/exercises/13/lsh_demo.py
@@ -1,17 +1,40 @@
 import numpy as np
+import cv2 as cv
 
 
-def lsh(v, C, seg_map_set):
+def feature_vector(img):
+    half_height = img.shape[0] // 2
+    half_width = img.shape[1] // 2
+    # divide into 4 regions
+    regions = (img[:half_height, :half_width], img[:half_height, half_width:],
+               img[half_height:, :half_width], img[half_height:, half_width:])
+    hists = []
+    for r in regions:
+        # sum of intensity of each color
+        hist = np.array([r[:, :, i].sum() for i in range(3)], np.float64)
+        hists.append(hist / hist.sum())
+    v = np.concatenate(hists)
+    for i in range(v.shape[0]):
+        if v[i] < 0.3:
+            v[i] = 0
+        elif v[i] < 0.6:
+            v[i] = 1
+        else:
+            v[i] = 2
+    return v.astype(np.uint8)
+
+
+def lsh(v, seg_map_set):
     """
     Locality Sensitive Hashing
     :param v: vector
-    :param C: code length
     :param seg_map_set: segmented map set
     :return: int
     """
     result = ''
     for i, s in enumerate(seg_map_set):
         if len(s):
+            # count elements in s that are less than v[i]
             count = 0
             for x in s:
                 if x <= v[i]:
@@ -19,6 +42,7 @@ def lsh(v, C, seg_map_set):
                 else:
                     break
             result += '1' * count + '0' * (len(s) - count)
+    # represent the hash value as a decimal
     return int(result, 2)
 
 
@@ -26,8 +50,9 @@ class LSHTable:
     def __init__(self, map_sets, C, dim):
         self.C = C
         self.dim = dim
-        self.table = []
-        print(self.segment_map_set(map_sets))
+        self.map_sets = [self.segment_map_set(s) for s in map_sets]
+        # Each dict is a hash bin.
+        self.table = [dict() for _ in range(len(map_sets))]
 
     def segment_map_set(self, map_set):
         """
@@ -45,13 +70,57 @@ class LSHTable:
                 else:
                     break
                 j += 1
+            # An empty list is appended, like an empty set.
             segmented.append(segment)
         return segmented
 
+    def add(self, v):
+        """Add a vector to the hash table
+        :param v: vector
+        """
+        for i, s in enumerate(self.map_sets):
+            h = lsh(v, s)
+            if self.table[i].get(h) is None:
+                self.table[i][h] = [v]
+            else:
+                self.table[i][h].append(v)
+
+    def knn(self, v, k):
+        """
+        Find k nearest neighbors
+        :param v: vector
+        :param k: number of neighbors
+        :return: list
+        """
+        candidates = []
+        for i, s in enumerate(self.map_sets):
+            # for each vector in the same bin
+            for c in self.table[i][lsh(v, s)]:
+                # append to candidates if not a duplicate
+                for appended_candidate in candidates:
+                    if np.array_equal(appended_candidate[0], c):
+                        break
+                else:
+                    # second item is the Euclidean distance
+                    candidates.append((c, np.sqrt(np.square(v.astype(np.float64) - c).sum())))
+        candidates.sort(key=lambda c: c[1])
+        if len(candidates) > k:
+            return [candidates[i][0] for i in range(k)]
+        else:
+            return [c[0] for c in candidates]
+
 
 if __name__ == '__main__':
-    # p = np.array([2, 4, 3, 5], np.uint8)
-    # I = [[1, 2, 3, 5], [2, 3], [], []]
-    # C = 5
-    # print(lsh(p, C, I))
-    t = LSHTable([1, 2, 3, 5, 7, 8], 5, 4)
+    map_sets = [
+        [2, 4, 11, 13, 16, 17, 23, 24], [1, 6, 13, 16, 18, 19, 21, 24],
+        [1, 4, 12, 14, 16, 17, 19, 21], [2, 3, 5, 12, 14, 18, 21, 23],
+        [3, 5, 6, 9, 13, 19, 20, 22]
+    ]
+    t = LSHTable(map_sets, 2, 12)
+    for i in range(1, 41):
+        filename = 'Dataset/{}.jpg'.format(i)
+        img = cv.imread(filename)
+        t.add(feature_vector(img))
+    img = cv.imread('target.jpg')
+    v = feature_vector(img)
+    print(t.knn(v, 8))
